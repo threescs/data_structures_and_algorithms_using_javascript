854854aed3c44c7110c61f49a7750eaf85cb4e5f
shangchengshuai <threescs@163.com>
2019-08-09 14:24:40 +0800

    add 递归 && 栈模拟递归
    add 递归 && 栈模拟递归

--- 栈的实现.js
+++ 栈的实现.js
@@ -22,3 +22,35 @@ function length() {
 function clear() {
     this.top = 0;
 }
+
+// 回文, 判断给定字符串是否是回文
+function isPalidrome(word) {
+    var s = new Stack();
+    for (var i = 0; i < word.length; ++i) {
+        s.push(word[i]);
+    }
+    var rword = '';
+    while(s.length() > 0) {
+        rword += s.pop();
+    }
+    if(word == rword) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+var word = 'hello';
+if(isPalidrome(word)) {
+    console.log(word + ' is a palindrome.');
+}
+else {
+    console.log(word + ' is not a palindrome.');
+}
+word = 'racecar';
+if(isPalidrome(word)) {
+    console.log(word + ' is a palindrome.');
+}
+else {
+    console.log(word + ' is not a palindrome');
+}
---
+++ 递归.js
@@ -0,0 +1,8 @@
+function factorial(n) {
+    if (n === o) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
